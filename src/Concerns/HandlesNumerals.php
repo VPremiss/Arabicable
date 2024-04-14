7cdf237a1575327e683fc108c8d05f4a60a015d8
GoodM4ven <GoodM4ven@users.noreply.github.com>
2024-04-14 12:21:14 +0000

    Style with Pint

--- src/Concerns/HandlesNumerals.php
+++ src/Concerns/HandlesNumerals.php
@@ -5,6 +5,7 @@ namespace VPremiss\Arabicable\Concerns;
 trait HandlesNumerals
 {
     public const INDIAN_NUMERALS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
     public const ARABIC_NUMERALS = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
 
     public function convertNumeralsToIndian(string $text): string
