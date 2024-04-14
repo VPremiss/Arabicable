cb16e7e03a022a86e3b508714bc696797b1f1813
GoodM4ven <GoodM4ven@proton.me>
2024-04-14 15:32:39 +0300

    Wrote doc-block intellisense helpers for the facades

--- src/Facades/Arabic.php
+++ src/Facades/Arabic.php
@@ -4,7 +4,13 @@ namespace VPremiss\Arabicable\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+// TODO check if adding the other Text method docs is needed
+
 /**
+ * @method static string removeHarakat(string $text)
+ * @method static string normalizeHuroof(string $text)
+ * @method static string convertToFasila(string $text)
+ * 
  * @see \VPremiss\Arabicable\Arabic
  */
 class Arabic extends Facade
--- src/Facades/Text.php
+++ src/Facades/Text.php
@@ -5,6 +5,16 @@ namespace VPremiss\Arabicable\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string removePunctuationMarks(string $text)
+ * @method static string removeEnclosingMarks(string $text)
+ * @method static string convertNumeralsToIndian(string $text)
+ * @method static string convertNumeralsToArabicAndIndianSequences(string $text)
+ * @method static string deduplicateArabicAndIndianNumeralSequences(string $text)
+ * @method static string addSpacesAroundPunctuationMarks(string $text)
+ * @method static string addSpacesForColonsAfterDoubleQuotes(string $text)
+ * @method static string removeEmptySpaces(string $text)
+ * @method static string removeSpacesWithinQuotes(string $text)
+ * 
  * @see \VPremiss\Arabicable\Text
  */
 class Text extends Facade
