62289cfe62a7b79b4cac4bf600c6afeef31af71d
GoodM4ven <GoodM4ven@proton.me>
2024-04-14 20:37:28 +0300

    Added @method references to inherited methods and their traits

--- src/Facades/Arabic.php
+++ src/Facades/Arabic.php
@@ -4,12 +4,19 @@ namespace VPremiss\Arabicable\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-// TODO check if adding the other Text method docs is needed
-
 /**
  * @method static string removeHarakat(string $text)
  * @method static string normalizeHuroof(string $text)
  * @method static string convertToFasila(string $text)
+ * @method static string removePunctuationMarks(string $text)
+ * @method static string removeEnclosingMarks(string $text)
+ * @method static string convertNumeralsToIndian(string $text)
+ * @method static string convertNumeralsToArabicAndIndianSequences(string $text)
+ * @method static string deduplicateArabicAndIndianNumeralSequences(string $text)
+ * @method static string addSpacesAroundPunctuationMarks(string $text)
+ * @method static string addSpacesForColonsAfterDoubleQuotes(string $text)
+ * @method static string removeEmptySpaces(string $text)
+ * @method static string removeSpacesWithinQuotes(string $text)
  *
  * @see \VPremiss\Arabicable\Arabic
  */
