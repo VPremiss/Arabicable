94bde4c4e05f93e956eff93846e2fc01e2f1cd0b
GoodM4ven <goodm4ven@proton.me>
2024-04-24 16:31:24 +0300

    [v3.0.2] Integrating Crafty's installation command

--- src/ArabicableServiceProvider.php
+++ src/ArabicableServiceProvider.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace VPremiss\Arabicable;
 
-use Illuminate\Support\Facades\File;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use VPremiss\Arabicable\Concerns\HasArabicableMigrationBlueprintMacros;
 use VPremiss\Arabicable\Support\Concerns\HasValidatedConfiguration;
 use VPremiss\Crafty\Utilities\Configurated\Interfaces\Configurated;
+use VPremiss\Crafty\Utilities\Installable\Interfaces\Installable;
+use VPremiss\Crafty\Utilities\Installable\Traits\HasInstallationCommand;
 
-class ArabicableServiceProvider extends PackageServiceProvider implements Configurated
+class ArabicableServiceProvider extends PackageServiceProvider implements Installable, Configurated
 {
+    use HasInstallationCommand;
     use HasValidatedConfiguration;
     use HasArabicableMigrationBlueprintMacros;
 
@@ -27,69 +28,20 @@ class ArabicableServiceProvider extends PackageServiceProvider implements Config
         $package
             ->name('arabicable')
             ->hasConfigFile()
-            ->hasMigration('create_common_arabic_texts_table')
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command->publishConfigFile();
-
-                $command->publishMigrations();
-                $command->askToRunMigrations();
-
-                // ? Seeding common-Arabic-text into database
-                if (!app()->environment('testing')) {
-                    // Publish the seeder file
-                    $this->publishes([
-                        __DIR__ . '/../database/seeders/CommonArabicTextSeeder.php' => ($seederPath = database_path('seeders/CommonArabicTextSeeder.php')),
-                    ], 'arabicable-seeders');
-                    $command->publish('seeders');
-
-                    if (File::exists($seederPath)) {
-                        // Correct the seeder's namespace
-                        File::put(
-                            $seederPath,
-                            str_replace(
-                                'namespace VPremiss\Arabicable\Database\Seeders;',
-                                'namespace Database\Seeders;',
-                                File::get($seederPath),
-                            ),
-                        );
-                        // Seed into the database
-                        $command->startWith(
-                            fn (InstallCommand $command) => $command->call('db:seed', [
-                                '--class' => 'CommonArabicTextSeeder',
-                                '--force',
-                            ])
-                        );
-                    }
-
-                    // Prepend into the DatabaseSeeder for continuous migration upon `fresh`ing
-                    if (File::exists($databaseSeederPath = database_path("seeders/DatabaseSeeder.php"))) {
-                        $fileContents = File::get($databaseSeederPath);
-
-                        if (strpos($fileContents, "\$this->call(CommonArabicTextSeeder::class);") === false) {
-                            $searchPattern = strpos($fileContents, "public function run(): void\n    {") !== false ?
-                                "public function run(): void\n    {" :
-                                "public function run()\n    {";
-                            $replacePattern = $searchPattern === "public function run(): void\n    {" ?
-                                "public function run(): void\n    {\n        \$this->call(CommonArabicTextSeeder::class);" :
-                                "public function run()\n    {\n        \$this->call(CommonArabicTextSeeder::class);";
-                            $newContents = str_replace(
-                                $searchPattern,
-                                $replacePattern,
-                                $fileContents
-                            );
-
-                            File::put($databaseSeederPath, $newContents);
-                        }
-                    }
-                }
-
-                $command->askToStarRepoOnGitHub('vpremiss/arabicable');
-            });
+            ->hasMigration('create_common_arabic_texts_table');
     }
 
     public function bootingPackage()
     {
+        $this->installationCommand();
         $this->arabicableMigrationBlueprintMacros();
+    }
+
+    public function seederFilePaths(): array
+    {
+        return [
+            __DIR__ . '/../database/seeders/CommonArabicTextSeeder.php',
+        ];
     }
 
     public function configValidation(string $configKey): void
